93d2fe58da0ab8e3720d436983b73a7e740023ef
mdanyassri <72862261+mdanyassri@users.noreply.github.com>
2021-12-07 13:23:09 +0530

    Add files via upload

---
+++ Class100.py
@@ -0,0 +1,18 @@
+class Car(object):
+    def __init__(self, color, company, speed_limit): 
+        self.color = color 
+        self.company = company 
+        self.speed_limit = speed_limit 
+        self.model = model
+
+    def start(self): 
+        print("started")
+
+    def stop(self): 
+        print("stopped") 
+
+    def accelerate(self): 
+        print("accelarating...") 
+        
+    def change_gear(shelf, gear_type): 
+        print("gear changed")
